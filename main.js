8e8be9f3226da9b79b35138aafcdd04438df0148
jessikagabi <161604165+jessikagabi@users.noreply.github.com>
2025-05-06 11:44:07 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -15,6 +15,15 @@ for (let i=0; i < botoes.length; i++){
 
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
-let tempoAtual = new Date();
 
-contadores[0].textContent = tempoObjetivo1;
+contadores[0].textContent = calculaTempo(tempoObjetivo1);{
+    let tempoAtual = new Date();
+    let tempoFinal = tempoObjetivo - tempoAtual;
+    let segundos = Math.floor (tempoFinal/1000);
+    let minutos = Math.floor (segundos/60);
+    let horas = Math.floor (minutos/60);
+    let dias = Math.floor 
+
+
+    return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos ";
+}
